bbb8fbd09faf81b37390d1cff84538227ff41302
Onorio Catenacci <catenacci@ieee.org>
2023-08-09 14:38:56 -0400

    Add tailwind to project
    Add tailwind 3.3.3 to the project.

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :donek, DonekWeb.Endpoint,
   secret_key_base: "xY4AF7DzZCM2js9Pjje6Z/E8UqPAIWrlLYo2Tsi3eENv4WY/106K5ZYDxRYTAMNO",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
--- lib/donek_web/live/newtabledefinition.ex
+++ lib/donek_web/live/newtabledefinition.ex
@@ -13,6 +13,16 @@ defmodule DonekWeb.NewTableDefinitionLive do
       <label for="table_name">Table Name</label>
       <input id="table_name" type="text" />
     </div>
+    <div>
+      <label for="table_description">Table Description</label>
+      <input id="table_description" type="text" />
+    </div>
+    </section>
+    <section class="field_definitions">
+    <div>
+      <label for="field_name">Field Name</label>
+      <input id="field_name" type="text" />
+    </div>
     </section>
     """
   end
